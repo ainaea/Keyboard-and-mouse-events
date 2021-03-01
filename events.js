2961e29192d1fc4116157468a95aed003f208d27
ainaea <ebenezer.a.aina@gmail.com>
2021-03-01 05:53:21 +0100

    done task

---
+++ events.js
@@ -0,0 +1,14 @@
+
+function mouseEvent(obj){
+    var chosen = document.getElementById(obj);
+    chosen.style.color = "red";
+    
+}
+
+
+function keyEvent(obj){
+    var chosen = document.getElementById(obj);
+    chosen.style.background = "red";
+    chosen.style.color = "white";
+    
+}
---
+++ greet.js
@@ -0,0 +1,16 @@
+function greetAll(userName){
+    return "Welcome here " + userName;
+
+}
+
+function greetSelected(userName){
+    if (userName == "Alice" || "Bob") {
+        return "Welcome here " + userName;    
+    }
+}
+
+function setInner(iden){
+    // var sources = document.getElementById(iden);
+    // var destination = document.getElementById("inputKeyPress1");
+    // var user = source
+}
